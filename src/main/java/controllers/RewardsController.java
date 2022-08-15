bb75e456ca00780fc6d3cf7570ddfd7fa63bec2d
samanthamebius <79612950+samanthamebius@users.noreply.github.com>
2022-08-15 12:37:41 +1200

    Establish basic project structure (#20)
    * add stage attributes
    
    * add main project classes

---
+++ src/main/java/controllers/RewardsController.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class RewardsController extends SceneController {
+
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.a1project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.a1project to javafx.fxml;
-    exports com.example.a1project;
-}
