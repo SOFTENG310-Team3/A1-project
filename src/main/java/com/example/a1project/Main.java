c3891d830c1461534a507afbb542104fd4561586
samanthamebius <smeb890@aucklanduni.ac.nz>
2022-08-15 12:15:44 +1200

    add stage attributes

---
+++ src/main/java/com/example/a1project/Main.java
@@ -0,0 +1,30 @@
+package com.example.a1project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+	
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    	Group root = new Group();
+    	Scene scene = new Scene(root);
+    	
+    	stage.setTitle("My To Do's");
+    	stage.setWidth(1200);
+    	stage.setHeight(800);
+    	stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
