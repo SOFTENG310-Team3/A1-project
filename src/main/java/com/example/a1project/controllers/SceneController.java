6e3cb23ef3c7ece1df720f20829cdb4bf989b03b
samanthamebius <smeb890@aucklanduni.ac.nz>
2022-08-17 09:42:02 +1200

    added navigation to rewards screen

--- src/main/java/com/example/a1project/controllers/SceneController.java
+++ src/main/java/com/example/a1project/controllers/SceneController.java
@@ -22,6 +22,14 @@ public class SceneController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void switchToRewards(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("../Rewards.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 	
 }
