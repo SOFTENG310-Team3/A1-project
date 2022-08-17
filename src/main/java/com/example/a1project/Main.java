ce555136f826bc8fd2fe1e7a96bc0a12da883993
Patrick Oliver <poli648@aucklanduni.ac.nz>
2022-08-17 18:23:19 +1200

    Touched up css of tack screen and reverted main to before testing

--- src/main/java/com/example/a1project/Main.java
+++ src/main/java/com/example/a1project/Main.java
@@ -2,11 +2,9 @@ package com.example.a1project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -15,18 +13,19 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         
-    	Parent root = FXMLLoader.load(getClass().getResource("Task.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
     	Scene scene = new Scene(root);
     	
-    	String css = this.getClass().getResource("Task.css").toExternalForm();
+    	String css = this.getClass().getResource("ToDO.css").toExternalForm();
     	scene.getStylesheets().add(css);
     	
     	stage.setTitle("My To Do's");
-    	//stage.setWidth(1200);
-    	//stage.setHeight(800);
+    	stage.setWidth(1200);
+    	stage.setHeight(800);
     	stage.setResizable(false);
+  
         stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT);
+        
         stage.show();
     }
 
