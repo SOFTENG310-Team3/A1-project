7107e969dd29ce13cffeb986c4aebd34acb3bd39
Patrick Oliver <poli648@aucklanduni.ac.nz>
2022-08-17 16:03:53 +1200

    created the basic layout of the task GUI, added gave fields ID and removed title bar

--- src/main/java/com/example/a1project/Main.java
+++ src/main/java/com/example/a1project/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,17 +15,18 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         
-    	Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("Task.fxml"));
     	Scene scene = new Scene(root);
     	
-    	String css = this.getClass().getResource("ToDo.css").toExternalForm();
-    	scene.getStylesheets().add(css);
+    	//String css = this.getClass().getResource("ToDo.css").toExternalForm();
+    	//scene.getStylesheets().add(css);
     	
     	stage.setTitle("My To Do's");
-    	stage.setWidth(1200);
-    	stage.setHeight(800);
+    	//stage.setWidth(1200);
+    	//stage.setHeight(800);
     	stage.setResizable(false);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
 
