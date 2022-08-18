d1eb8ded6e6f0f35a59073c98c577080c17d3c2e
samanthamebius <smeb890@aucklanduni.ac.nz>
2022-08-19 10:16:34 +1200

    refactored test suite to reuse achievement object across test cases

--- src/main/java/com/example/a1project/TestSuite.java
+++ src/main/java/com/example/a1project/TestSuite.java
@@ -2,57 +2,53 @@ package com.example.a1project;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSuite {
 	
 	// Achievement Class Tests
 	
-	@Test
-	public void testAcheivementConstructor() {
+	private Achievement ach;
+	
+	@Before
+	public void setUpAchievements() {
+		
 		String description = "Complete 10 tasks";
 		boolean isUnlocked = false;
 		AchievementMetric metric = AchievementMetric.TASKS_COMPLETE;
 		int requiredVal = 10;
 		int currentVal = 0;
 		
-		Achievement ach = new Achievement(description, isUnlocked, metric, requiredVal, currentVal);
+		ach = new Achievement(description, isUnlocked, metric, requiredVal, currentVal);
 		
-		assertEquals(ach.getDescription(),description);
-		assertEquals(ach.getIsUnlocked(),isUnlocked);
-		assertEquals(ach.getMetric(),metric);
-		assertEquals(ach.getRequiredMetricVal(),requiredVal);
-		assertEquals(ach.getCurrentMetricVal(),currentVal);
+	}
+	
+	@Test
+	public void testAcheivementConstructor() {
+		
+		assertEquals(ach.getDescription(),"Complete 10 tasks");
+		assertEquals(ach.getIsUnlocked(),false);
+		assertEquals(ach.getMetric(),AchievementMetric.TASKS_COMPLETE);
+		assertEquals(ach.getRequiredMetricVal(),10);
+		assertEquals(ach.getCurrentMetricVal(),0);
+		
 	}
 	
 	@Test
 	public void testSetCurrentMetricVal() {
-		String description = "Complete 10 tasks";
-		boolean isUnlocked = false;
-		AchievementMetric metric = AchievementMetric.TASKS_COMPLETE;
-		int requiredVal = 10;
-		int currentVal = 0;
 		
-		Achievement ach = new Achievement(description, isUnlocked, metric, requiredVal, currentVal);
-	
 		ach.setCurrentMetricVal(2);
-		
 		assertEquals(ach.getCurrentMetricVal(),2);
+		
 	}
 	
 	@Test
 	public void testSetisUnlocked() {
-		String description = "Complete 10 tasks";
-		boolean isUnlocked = false;
-		AchievementMetric metric = AchievementMetric.TASKS_COMPLETE;
-		int requiredVal = 10;
-		int currentVal = 0;
-		
-		Achievement ach = new Achievement(description, isUnlocked, metric, requiredVal, currentVal);
 	
 		ach.setIsUnlocked(true);
-		
 		assertEquals(ach.getIsUnlocked(),true);
+		
 	}
 	
 
