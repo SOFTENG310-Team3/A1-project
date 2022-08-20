3a2f378df34e37824bf5541d9ba6841c8f69c5b0
samanthamebius <79612950+samanthamebius@users.noreply.github.com>
2022-08-20 18:03:32 +1200

    Created achievements class (#41)
    * added basic fields and methods to achievement class
    
    * refactor metric field to use an enum
    
    * added test cases for achievement class
    
    * refactored test suite to reuse achievement object across test cases

--- src/main/java/com/example/a1project/Achievement.java
+++ src/main/java/com/example/a1project/Achievement.java
@@ -1,5 +1,49 @@
 package com.example.a1project;
 
 public class Achievement {
+	
+	private final String description;
+	private boolean isUnlocked;
+	private final AchievementMetric metric; 
+	private final int requiredMetricVal; 
+	private int currentMetricVal; 
+	
+
+	public Achievement(String description, boolean isUnlocked, AchievementMetric metric, int requiredMetricVal, int currentMetricVal) {
+		this.description = description;
+		this.isUnlocked = isUnlocked;
+		this.metric = metric;
+		this.requiredMetricVal = requiredMetricVal;
+		this.currentMetricVal = currentMetricVal;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public boolean getIsUnlocked() {
+		return this.isUnlocked;
+	}
+	
+	public AchievementMetric getMetric() {
+		return this.metric;
+	}
+	
+	public int getRequiredMetricVal() {
+		return this.requiredMetricVal;
+	}
+	
+	public int getCurrentMetricVal () {
+		return this.currentMetricVal;
+	}
+	
+	public void setIsUnlocked(boolean status) {
+		this.isUnlocked = status;
+	}
+	
+	public void setCurrentMetricVal(int newVal) {
+		this.currentMetricVal = newVal;
+	}
+	
 
 }
---
+++ src/main/java/com/example/a1project/AchievementMetric.java
@@ -0,0 +1,5 @@
+package com.example.a1project;
+
+public enum AchievementMetric {
+	TASKS_COMPLETE, TASKS_COMPLETE_ON_TIME
+}
