7ceceb22b56ef54b25f16fdbb0bc4ff0eb86f801
mmcc944 <79692780+mmcc944@users.noreply.github.com>
2022-08-21 21:09:32 +1200

    Initial set of achievements showing (#48)
    * Created a small list of achievements and got them showing on the rewards screen
    
    * Attempted fix at error sonarcube discovered
    
    * Moved txt files to better location

--- src/main/java/com/example/a1project/controllers/RewardsController.java
+++ src/main/java/com/example/a1project/controllers/RewardsController.java
@@ -1,5 +1,23 @@
 package com.example.a1project.controllers;
 
-public class RewardsController extends SceneController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import com.example.a1project.Utility;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class RewardsController extends SceneController implements Initializable {
+
+	@FXML 
+	private ListView<String> achievementList;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		String[] achievements = Utility.getAchievementsToList();
+		achievementList.getItems().addAll(achievements);
+	}
+	
 }
