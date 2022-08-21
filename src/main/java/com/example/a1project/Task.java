c7f048bfc808a0eb8087d8b6c9bcb9f1f5fc652b
jzho987 <jzho987@aucklanduni.ac.nz>
2022-08-22 00:27:25 +1200

    feat: added variables and setters/getters
    added all required variables and their respective setters and getters for the Task class.

--- src/main/java/com/example/a1project/Task.java
+++ src/main/java/com/example/a1project/Task.java
@@ -2,4 +2,111 @@ package com.example.a1project;
 
 public class Task {
 
+	private String description;
+	private String location;
+	private String category;
+	private String frequency;
+	private String dueDate;
+	private String dueTime;
+	
+	private int priority;
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	/**
+	 * @return the frequency
+	 */
+	public String getFrequency() {
+		return frequency;
+	}
+
+	/**
+	 * @param frequency the frequency to set
+	 */
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	/**
+	 * @return the dueDate
+	 */
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	/**
+	 * @param dueDate the dueDate to set
+	 */
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	/**
+	 * @return the dueTime
+	 */
+	public String getDueTime() {
+		return dueTime;
+	}
+
+	/**
+	 * @param dueTime the dueTime to set
+	 */
+	public void setDueTime(String dueTime) {
+		this.dueTime = dueTime;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
 }
