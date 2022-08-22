dd1f0a6f295e0d17cb30bc3eaf50dca74f45187a
jzho987 <jzho987@aucklanduni.ac.nz>
2022-08-22 22:49:53 +1200

    feat: added test for task display

--- src/main/java/com/example/a1project/controllers/MainScreenController.java
+++ src/main/java/com/example/a1project/controllers/MainScreenController.java
@@ -1,17 +1,20 @@
 package com.example.a1project.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.example.a1project.Task;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
-public class MainScreenController extends SceneController {
+public class MainScreenController extends SceneController implements Initializable {
 	
     @FXML
     private GridPane currentTaskGridPane;
@@ -38,9 +41,7 @@ public class MainScreenController extends SceneController {
 		try {
 			FXMLLoader fxmlloader = new FXMLLoader();
 			fxmlloader.setLocation(getClass().getResource("/com/example/a1project/SingleTaskDisplay.fxml"));
-			AnchorPane anchor;
-			
-			anchor = fxmlloader.load();
+			AnchorPane anchor = fxmlloader.load();
 			
 			SingleTaskDisplayController taskDisplay = fxmlloader.getController();
 			taskDisplay.setData(task);
@@ -50,5 +51,13 @@ public class MainScreenController extends SceneController {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Task task = new Task("TEST_TASK_DESCRIPTION", "LOCATION", "CATEGORY", "FREQUENCY", "DUEDATE", "DUETIME",3);
+		
+		newCurrentTaskDisplay(task);
+		
 	}
 }
