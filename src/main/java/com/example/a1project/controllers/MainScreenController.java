1aed3a82d21948354af26026182c474b218d1e9d
jzho987 <jzho987@aucklanduni.ac.nz>
2022-08-22 22:37:18 +1200

    feat: added grid panes for task display

--- src/main/java/com/example/a1project/controllers/MainScreenController.java
+++ src/main/java/com/example/a1project/controllers/MainScreenController.java
@@ -2,10 +2,23 @@ package com.example.a1project.controllers;
 
 import java.io.IOException;
 
+import com.example.a1project.Task;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class MainScreenController extends SceneController {
+	
+    @FXML
+    private GridPane currentTaskGridPane;
 
+    @FXML
+    private GridPane completedTaskGridPane;
+    
 	public void addTask(ActionEvent event) throws IOException {
 		showNewTaskPopup(event);
 	}
